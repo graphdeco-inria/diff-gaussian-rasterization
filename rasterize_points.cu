feecabda8a0cd12b3712c8d231bf4ea859d745c6
Bernhard Kerbl <bkerbl@ad.inria.fr>
2023-06-28 00:52:17 +0200

    It's not true!

--- rasterize_points.cu
+++ rasterize_points.cu
@@ -1,5 +1,3 @@
-// Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
-
 #include <math.h>
 #include <torch/extension.h>
 #include <cstdio>
--- rasterize_points.h
+++ rasterize_points.h
@@ -1,5 +1,3 @@
-// Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
-
 #pragma once
 #include <torch/extension.h>
 #include <cstdio>
