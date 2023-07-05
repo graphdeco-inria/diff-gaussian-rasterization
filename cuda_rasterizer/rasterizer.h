4aedd8226f7257935891049f5a378b0e21d0aa37
Bernhard Kerbl <kerbl@icg.tugraz.at>
2023-07-05 12:19:47 +0200

    Added license files

--- cuda_rasterizer/rasterizer.h
+++ cuda_rasterizer/rasterizer.h
@@ -1,3 +1,14 @@
+/*
+ * Copyright (C) 2023, Inria
+ * GRAPHDECO research group, https://team.inria.fr/graphdeco
+ * All rights reserved.
+ *
+ * This software is free for non-commercial, research and evaluation use 
+ * under the terms of the LICENSE.md file.
+ *
+ * For inquiries contact  george.drettakis@inria.fr
+ */
+
 #ifndef CUDA_RASTERIZER_H_INCLUDED
 #define CUDA_RASTERIZER_H_INCLUDED
 
