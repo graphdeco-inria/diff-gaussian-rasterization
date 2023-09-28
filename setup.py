1e751edfe260b04a5f5ba34b02062a5fb227ddfa
Shun-Cheng Wu <23650527+ShunChengWu@users.noreply.github.com>
2023-09-28 21:50:31 +0200

    fix the error of illegal memory access caused by cub

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
             "cuda_rasterizer/backward.cu",
             "rasterize_points.cu",
             "ext.cpp"],
-            extra_compile_args={"nvcc": ["-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "third_party/glm/")]})
+            extra_compile_args={"nvcc": ["-Xcompiler", "-fno-gnu-unique","-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "third_party/glm/")]})
         ],
     cmdclass={
         'build_ext': BuildExtension
