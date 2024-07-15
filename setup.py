a27752ff7b03841eca617f997b5e5ce7a590977e
BoenJiang <jbelove@qq.com>
2024-07-15 16:42:17 +0800

    1.【修改】将包的名称改为“volumn_splatting_rasterization”；

--- setup.py
+++ setup.py
@@ -15,11 +15,11 @@ import os
 os.path.dirname(os.path.abspath(__file__))
 
 setup(
-    name="diff_gaussian_rasterization",
-    packages=['diff_gaussian_rasterization'],
+    name="volumn_splatting_rasterization",
+    packages=['volumn_splatting_rasterization'],
     ext_modules=[
         CUDAExtension(
-            name="diff_gaussian_rasterization._C",
+            name="volumn_splatting_rasterization._C",
             sources=[
             "cuda_rasterizer/rasterizer_impl.cu",
             "cuda_rasterizer/forward.cu",
